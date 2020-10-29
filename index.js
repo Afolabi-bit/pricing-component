ab04b7671a828b973fb73b6ee967c9d3a2e1143f
Joseph Afolabi <maverickoluwatomisin@gmail.com>
2020-10-29 23:49:23 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const button = document.getElementById('toggle-btn')
+const main = document.getElementById('main')
+
+
+
+
+button.addEventListener('click', ()=> {
+    main.classList.toggle('change');
+
+    const card1 = document.querySelector('#card-1 h2')
+    const card2 = document.querySelector('#card-2 h2')
+    const card3 = document.querySelector('#card-3 h2')
+})
